8779bf2f04a849ec2e3b75e2e316546999538a38
Phoebe Hugh <phoebehugh@gmail.com>
2015-03-23 12:26:21 +0000

    Airport has a default and custom capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,9 @@
 class Airport
+  DEFAULT_CAPACITY = 10
+
+  attr_reader :capacity
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,4 +5,9 @@ describe Airport do
     luton = Airport.new
     expect(luton.capacity).to eq 10
   end
+
+  it "can have a custom capacity" do
+    luton = Airport.new(capacity: 20)
+    expect(luton.capacity).to eq 20
+  end
 end
